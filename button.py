67c065089e55b96e4b1efd898d8462a8daad719d
Mathou0370 <125092392+Mathou0370@users.noreply.github.com>
2023-04-05 09:46:55 +0200

    git

---
+++ button.py
@@ -0,0 +1,64 @@
+import pygame
+
+
+buttons = []
+
+class Button():
+	def __init__(self, text, width, height, pos, elevation):
+		#Core attributes 
+		self.pressed = False
+		self.elevation = elevation
+		self.dynamic_elecation = elevation
+		self.original_y_pos = pos[1]
+
+		# top rectangle 
+		self.top_rect = pygame.Rect(pos, (width, height))
+		self.top_color = '#015E5E'
+
+		# bottom rectangle 
+		self.bottom_rect = pygame.Rect(pos, (width, height))
+		self.bottom_color = '#014A4A'
+		#text
+		# self.text_font = pygame.font.Font('./assets/font/mytype.ttf', 30)	#Font
+		self.text = text
+		# self.text_surf = self.text_font.render(text, True, '#FFFFFF')
+		# self.text_rect = self.text_surf.get_rect(center=self.top_rect.center)
+
+	def change_text(self, newtext):
+		self.text_surf = self.text_font.render(newtext, True, '#FFFFFF')
+		self.text_rect = self.text_surf.get_rect(center=self.top_rect.center)
+
+	def draw(self, screen):
+		# elevation logic 
+		self.top_rect.y = self.original_y_pos - self.dynamic_elecation
+		self.text_rect.center = self.top_rect.center 
+
+		self.bottom_rect.midtop = self.top_rect.midtop
+		self.bottom_rect.height = self.top_rect.height + self.dynamic_elecation
+
+		pygame.draw.rect(screen, self.bottom_color, self.bottom_rect, border_radius=12)
+		pygame.draw.rect(screen, self.top_color, self.top_rect, border_radius=12)
+		screen.blit(self.text_surf, self.text_rect)
+		self.check_click()
+
+	def check_click(self):
+		mouse = pygame.mouse.get_pos()
+		if self.top_rect.collidepoint(mouse):
+			self.top_color = '#02DADA'
+			if pygame.mouse.get_pressed()[0]:
+				self.dynamic_elecation = 0
+				self.pressed = True
+				self.change_text(f"{self.text}")
+			else:
+				self.dynamic_elecation = self.elevation
+				if self.pressed == True:
+					self.pressed = False
+					self.change_text(self.text)
+				
+		else:
+			self.dynamic_elecation = self.elevation
+			self.top_color = '#015E5E'
+
+def buttons_draw(screen):
+    for b in buttons:
+        b.draw(screen)
--- main.py
+++ main.py
@@ -1,37 +1,42 @@
 import pygame
-import sys 
+import button
 
+# pygame setup
 pygame.init()
 
-# user32 = ctypes.windll.user32 
-# screensize = user32.GetSystemMetrics(78)-100, user32.GetSystemMetrics(79)-100
-
-screen = pygame.display.set_mode()
-
-width = screen.get_width()
-height = screen.get_height()
-
-print(width, height)
-
-
-fenetre = pygame.display.set_mode(width, height) #taille de la fentre
+width, height = 400, 400
+screen = pygame.display.set_mode((width, height)) #si vide alors plein écran
 pygame.display.set_caption("Loup-Garou") #titre de la fenêtre
-
-
-
-fond = pygame.image.load("fond.jpg").convert() #image de fond
-fenetre.blit(fond, (0,0))
-perso = pygame.image.load("loup.png").convert_alpha()
-fenetre.blit(perso, (200,300))
+image = pygame.image.load("logo.jpg").convert()
+pygame.display.set_icon(image)
+clock = pygame.time.Clock()
 running = True
 
-while running:
+pygame.mouse.set_cursor(*pygame.cursors.arrow)
 
-    # Gestion des événements
+#création et gestion des boutons
+def buttons_draw(screen):
+    for b in buttons:
+        b.draw(screen)
+
+buttons = []
+button1 = button.Button('Resume', 200, 40, (width, height), 5)
+buttons.append(button1)
+
+while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    fenetre.blit(fond, (0,0))
+    
 
-    # Mise à jour de l'affichage
-    pygame.display.update()
+    screen.fill("purple")
+
+    # RENDER YOUR GAME HERE
+    
+
+
+    pygame.display.flip()
+
+    clock.tick(60)  # limits FPS to 60
+
+pygame.quit()
