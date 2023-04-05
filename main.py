93f1c41af13d1ff1f3f8f501ee241b6ec936925c
Mathou0370 <125092392+Mathou0370@users.noreply.github.com>
2023-04-05 08:39:38 +0200

    blbl

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 import pygame
+import ctypes 
 
 pygame.init()
 
-fenetre = pygame.display.set_mode((400, 400)) #taille de la fentre
+user32 = ctypes.windll.user32 
+screensize = user32.GetSystemMetrics(78), user32.GetSystemMetrics(79)
+
+taille = (screensize)
+fenetre = pygame.display.set_mode(screensize) #taille de la fentre
 pygame.display.set_caption("Loup-Garou") #titre de la fenêtre
 
+
+
 fond = pygame.image.load("fond.jpg").convert() #image de fond
+perso = pygame.image.load("loup.png").convert_alpha()
 
 running = True
+
 while running:
 
     # Gestion des événements
