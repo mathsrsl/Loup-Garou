9eccf170fa02e66e874b0e141b058ed29c778a4a
mathsrsl <101506457+mathsrsl@users.noreply.github.com>
2023-04-16 22:00:20 +0200

    ajout de la petite patoune

--- utils/button.py
+++ utils/button.py
@@ -16,15 +16,16 @@ def isClicked(screen, button):
     return False
 
 class Button():
-    def __init__(self, text, pos, width, height=70, elevation=8, idColor=0, fontSize=30, border_radius=20):
+    def __init__(self, text, pos, width, height=70, elevation=5, idColor=0, fontSize=25, border_radius=20):
         #Core attributes 
         self.bgColor = [
             ('#000000', '#A7A7A7', '#414141'), 
             ('#1997EA', '#101278', '#38B0FF'), 
-            ('#6C1FAD', '#290B47', '#9F2DFF')
-            
+            ('#6C1FAD', '#290B47', '#9F2DFF')   
         ]
         self.pressed = False
+        self.height = height
+        self.width = width
         self.elevation = elevation
         self.dynamic_elecation = elevation
         self.original_x_pos = pos[0]
@@ -35,16 +36,16 @@ class Button():
         
         #button color
         if self.idColor-1 > len(self.bgColor):
-            self.color = self.bgColor[0][0], self.bgColor[0][1], self.bgColor[0][2]
+            self.color = self.bgColor[0][0], self.bgColor[0][1], self.bgColor[0][2], 0
         else: 
-            self.color = self.bgColor[idColor][0], self.bgColor[idColor][1], self.bgColor[idColor][2]
+            self.color = self.bgColor[idColor][0], self.bgColor[idColor][1], self.bgColor[idColor][2], idColor
         
         #top rectangle
-        self.top_rect = pygame.Rect(pos, (width, height))
+        self.top_rect = pygame.Rect(pos, (self.width, self.height))
         self.top_color = self.color[0]
         
         #bottom rectangle
-        self.bottom_rect = pygame.Rect(pos, (width, height))
+        self.bottom_rect = pygame.Rect(pos, (self.width, self.height))
         self.bottom_color = self.color[1]
         
         #text
@@ -58,21 +59,28 @@ class Button():
         self.text_rect = self.text_surf.get_rect(center=self.top_rect.center)
         
     def draw(self, screen):
-        # elevation logic 
+        # elevation logic
+        self.top_rect.x = self.original_x_pos - (self.dynamic_elecation // 2)
         self.top_rect.y = self.original_y_pos - self.dynamic_elecation
-        self.top_rect.x = self.original_x_pos - self.dynamic_elecation
+        
         self.text_rect.center = self.top_rect.center 
     
-        self.bottom_rect.midtop = (self.top_rect.midtop[0] + self.dynamic_elecation, self.top_rect.midtop[1] + self.dynamic_elecation)
+        self.bottom_rect.midtop = (int(self.top_rect.midtop[0] + self.dynamic_elecation//2), self.top_rect.midtop[1] + self.dynamic_elecation)
         self.bottom_rect.height = self.top_rect.height
         pygame.draw.rect(screen, self.bottom_color, self.bottom_rect, border_radius=self.border_radius)
         pygame.draw.rect(screen, self.top_color, self.top_rect, border_radius=self.border_radius)
         screen.blit(self.text_surf, self.text_rect)
+        
+        self.pawImage = pygame.image.load(f'./assets/image/icon/patte_{self.color[3]}.png')
+        self.xPositionImage = self.original_x_pos + self.width - self.pawImage.get_size()[0] - self.dynamic_elecation
+        self.yPositionImage = self.original_y_pos + self.height - self.pawImage.get_size()[1] - self.dynamic_elecation
+        screen.blit(self.pawImage, (self.xPositionImage, self.yPositionImage))
+        
         self.check_click()
         
     def check_click(self):
         mouse = pygame.mouse.get_pos()
-        print(mouse) #---------------------------------------
+        # print(mouse) #---------------------------------------
         if self.top_rect.collidepoint(mouse):
             self.top_color = self.color[2]                
             if pygame.mouse.get_pressed()[0]:
